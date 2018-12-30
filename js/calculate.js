7898608e2133ed0af4d830368e3194a074eb87a2
Randeep Singh <randeeprattan90@gmail.com>
2018-12-30 18:23:52 +0000

    Removed jQuery dependency from calculate.js

--- js/calculate.js
+++ js/calculate.js
@@ -2,19 +2,20 @@ function calculateAmountDeducted(percentageOff, currentPrice){
   return (percentageOff/100) * currentPrice;
 }
 
-var current_price = $(".product-price-primary").attr("content");
-var current_discounted_price = $(".product-price-secondary");
+var current_price = document.getElementsByClassName("product-price-primary")[0].getAttribute("content");
+var current_discounted_price = document.getElementsByClassName("product-price-secondary");
 
 if(current_price){
     var amount_deducted = calculateAmountDeducted(selected_discount, current_price);
 
     var discount_price = current_price - amount_deducted;
 
-    current_discounted_price.replaceWith("<li class='price product-price-secondary'><span>" + selected_discount + "% off </span>&pound;" + discount_price.toFixed(2) + "</li>");
+	var new_price = '<span>' + selected_discount + '% off </span>&pound;' + discount_price.toFixed(2);
+
+	for(let x of current_discounted_price){
+		x.innerHTML = new_price;
+	}
+
 } else {
-
-      console.log('Original Price Not Found');
-
+	console.log('Original Price Not Found');
 }
-
-
