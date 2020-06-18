d296c12b92f823b66af7fa7c9c3cc0abf230dc2a
Randeep Singh <rand-singh@users.noreply.github.com>
2020-06-18 18:20:18 +0100

    Updated scripts
    Updated scripts to work with updated website changes

--- js/calculate.js
+++ js/calculate.js
@@ -1,15 +1,27 @@
-function calculateAmountDeducted(percentageOff, currentPrice){
-  return (percentageOff/100) * currentPrice;
+/**
+ *
+ * @param {*} percentageOff
+ * @param {*} currentPrice
+ */
+function calculateAmountDeducted(percentageOff, currentPrice) {
+	return (percentageOff / 100) * currentPrice;
 }
 
+<<<<<<< Updated upstream
 var current_price = document.getElementsByClassName("product-price-primary")[0].getAttribute("content");
 var current_discounted_price = document.getElementsByClassName("product-price-secondary");
 
 if(current_price){
     var amount_deducted = calculateAmountDeducted(selected_discount, current_price);
+=======
+var current_price = document
+	.querySelector("section.pdp-pricing-module ul li")
+	.getAttribute("content");
+>>>>>>> Stashed changes
 
-    var discount_price = current_price - amount_deducted;
+var current_discounted_price = $(".product-price-secondary");
 
+<<<<<<< Updated upstream
 	var new_price = '<span>' + selected_discount + '% off </span>&pound;' + discount_price.toFixed(2);
 
 	for(let x of current_discounted_price){
@@ -18,4 +30,23 @@ if(current_price){
 
 } else {
 	console.log('Original Price Not Found');
+=======
+if (current_price) {
+	var amount_deducted = calculateAmountDeducted(
+		selected_discount,
+		current_price
+	);
+
+	var discount_price = current_price - amount_deducted;
+
+	current_discounted_price.replaceWith(
+		"<li class='price product-price-secondary'><h2><span>" +
+			selected_discount +
+			"% off </span>&pound;" +
+			discount_price.toFixed(2) +
+			"</h2></li>"
+	);
+} else {
+	console.log("Original Price Not Found");
+>>>>>>> Stashed changes
 }
--- js/discount.js
+++ js/discount.js
@@ -1,8 +1,9 @@
 function getSavedDiscount(savedDiscount, callback) {
-  chrome.storage.sync.get(savedDiscount, (items) => {
-    callback(chrome.runtime.lastError ? null : items[savedDiscount]);
-  });
+	chrome.storage.sync.get(savedDiscount, (items) => {
+		callback(chrome.runtime.lastError ? null : items[savedDiscount]);
+	});
 }
+<<<<<<< Updated upstream
  
 function calculateAmountDeducted(percentageOff, currentPrice){
   return (percentageOff/100) * currentPrice;
@@ -37,6 +38,51 @@ function init(){
       calculateNewPrice(10);
     }
   });
+=======
+
+function calculateAmountDeducted(percentageOff, currentPrice) {
+	return (percentageOff / 100) * currentPrice;
+}
+
+function calculateNewPrice(savedDiscount) {
+	var discount_amount = savedDiscount;
+
+	var product_price_container = document.querySelector(
+		"section.pdp-pricing-module ul li"
+	);
+
+	var current_price = product_price_container.getAttribute("content");
+
+	if (current_price) {
+		var amount_deducted = calculateAmountDeducted(
+			discount_amount,
+			current_price
+		);
+
+		var discount_price = current_price - amount_deducted;
+
+		product_price_container.insertAdjacentHTML(
+			"beforeend",
+			"<li class='price product-price-secondary'><h2><span>" +
+				discount_amount +
+				"% off </span>&pound;" +
+				discount_price.toFixed(2) +
+				"</h2></li>"
+		);
+	} else {
+		console.log("Original Price Not Found");
+	}
+}
+
+function init() {
+	getSavedDiscount("discountSavedInMemory", (savedDiscount) => {
+		if (savedDiscount) {
+			calculateNewPrice(savedDiscount);
+		} else {
+			calculateNewPrice(10);
+		}
+	});
+>>>>>>> Stashed changes
 }
 
 document.addEventListener("DOMContentLoaded", init());
