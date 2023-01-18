62bf0bc505a345c6172ddf6c6c1128a1db360628
Randeep Singh <rand-singh@users.noreply.github.com>
2023-01-18 00:56:21 +0000

    Update discount.js

--- js/discount.js
+++ js/discount.js
@@ -1,73 +1,58 @@
 function getSavedDiscount(savedDiscount, callback) {
 	chrome.storage.sync.get(savedDiscount, (items) => {		
-		callback(chrome.runtime.lastError ? null : items[savedDiscount]);
-	});
+		callback(chrome.runtime.lastError ? null : items[savedDiscount])
+	})
 }
 
 function calculateAmountDeducted(percentageOff, currentPrice) {
-	return (percentageOff / 100) * currentPrice;
+	return (percentageOff / 100) * currentPrice
 }
 
 function calculateNewPrice(savedDiscount) {
-	var discount_amount = savedDiscount;
-
-	var product_price_container = document.querySelector(
-		"section.pdp-pricing-module ul li"
-	);
-
-	var current_price = product_price_container.getAttribute("content");
+	const discount_amount = savedDiscount
+	const product_price_container = document.querySelector('section.pdp-pricing-module ul li')
+	const current_price = product_price_container.getAttribute('content')
 
 	if (current_price) {
-		var amount_deducted = calculateAmountDeducted(
-			discount_amount,
-			current_price
-		);
+		const amount_deducted = calculateAmountDeducted(discount_amount, current_price)
+		const discount_price = current_price - amount_deducted
 
-		var discount_price = current_price - amount_deducted;
-
-		product_price_container.insertAdjacentHTML(
-			"beforeend",
-			"<h2 class='product-price-discounted'><span>" +
-				discount_amount +
-				"% off </span>&pound;" +
-				discount_price.toFixed(2) +
-				"</h2>"
-		);
+		if (!product_price_container.querySelector('h2.product-price-discounted')) {
+			product_price_container.insertAdjacentHTML(
+				'beforeend',
+				`
+					<h2 class='product-price-discounted'>
+						<span>${discount_amount}% off </span>&pound;${discount_price.toFixed(2)}
+					</h2>
+				`
+			)
+		}
 	} else {
-		console.log("Original Price Not Found");
+		console.log('Original Price Not Found')
 	}
 }
 
 function init() {
-	const targetNode = document.getElementById("app")
-
+	const targetNode = document.getElementById('app')
     const config = {
-        attributes: true, 
-        childList: true, 
+        attributes: true,
+        childList: true,
         characterData: true,
-        subtree: true 
-    };
-
-    const callback = mutations => {  
-        mutations.forEach(mutation => {
-
-            if (mutation.type === 'childList') {
-                if (mutation.target.classList?.contains('pdp-pricing-module')) {
-					getSavedDiscount("discountSavedInMemory", (savedDiscount) => {
-						if (savedDiscount) {
-							calculateNewPrice(savedDiscount);
-						} else {
-							calculateNewPrice(10);
-						}
-					});
-                }
-            }
-        });
+        subtree: true
     }
 
-    const observer = new MutationObserver(callback);
+    const callback = mutations => {
+        mutations.forEach(mutation => {
+            if (mutation.type === 'childList') {
+                if (mutation.target.classList?.contains('pdp-pricing-module') || mutation.target.classList.contains('xs-block')) {
+					getSavedDiscount('discountSavedInMemory', (savedDiscount) => savedDiscount ? calculateNewPrice(savedDiscount) : calculateNewPrice(10))
+                }
+            }
+        })
+    }
 
-    observer.observe(targetNode, config);
+    const observer = new MutationObserver(callback)
+    observer.observe(targetNode, config)
 }
 
-document.addEventListener("DOMContentLoaded", init());
+document.addEventListener('DOMContentLoaded', init())
